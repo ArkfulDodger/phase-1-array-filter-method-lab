c6c6cfbda5019295e6f8ff2db28a2d8d0d7fe21b
Noah Reece <noah.w.reece@gmail.com>
2022-02-24 12:46:57 -0500

    commit tree 2267207666180074e58bfec319ac4a8448dafd2b

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Code your solution here
+function findMatching(array, str) {
+    return array.filter( item => item.toLowerCase() === str.toLowerCase());
+}
+
+function fuzzyMatch(array, str) {
+    let len = str.length;
+    return array.filter ( item => item.slice(0, len) === str);
+}
+
+function matchName(objArray, str) {
+    return objArray.filter( item => item.name === str);
+}
